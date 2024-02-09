1d891e385ac9d8e2984cf001198b0844de82cbe4
Syed Dabeer Raza Zaidi <154671460+dabeer110@users.noreply.github.com>
2024-02-09 11:59:25 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,19 @@
 function newfunction(){
    let item = document.getElementById("input").value
    const ul = document.querySelector("ul")
-   ul.innerHTML += `<li> ${item}<button onclick="deleteit(this)">Delete</button>
-   <button onclick="updateit()">Update</button></li>`
+   ul.innerHTML += `<div><li> ${item}</li>
+   <button onclick="deleteit(this)">Delete</button>
+   <button onclick="updateit(this)">Update</button>
+   </div>`
 document.getElementById("input").value=("")
 
 }
 function deleteit(getObject){
     console.log(getObject)
     getObject.parentElement.remove()
+}
+function updateit(getobject){
+    getobject.parentElement.children[0].setAttribute("contentEditable",true)
 }
 
 
